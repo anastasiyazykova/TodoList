6a42a3d43f1493be843505856d6461e5f59dd5ad
Anastasiya Zykova <azalekss@mail.ru>
2020-04-23 21:18:51 +0700

    Fix problem

--- app/src/main/java/nsu/android/todolist/MyAdapter.java
+++ app/src/main/java/nsu/android/todolist/MyAdapter.java
@@ -3,43 +3,70 @@ package nsu.android.todolist;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.List;
 
+import nsu.android.todolist.Model.Task;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
-    private List<CoordinatorLayout> mDataset;
+    private List<Task> tasks;
 
-    public MyAdapter(List<CoordinatorLayout> myDataset) {
-        mDataset = myDataset;
+    public MyAdapter(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        CoordinatorLayout view = (CoordinatorLayout) LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.task_list_item, viewGroup, false);
-
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+        CoordinatorLayout view = (CoordinatorLayout) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.task_list_item, parent, false);
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int i) {
-        holder.coordinatorLayout = mDataset.get(i);
+        Task task = tasks.get(i);
+        holder.update(task);
     }
 
     @Override
     public int getItemCount() {
-        return mDataset.size();
+        return tasks.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        public CoordinatorLayout coordinatorLayout;
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView item_task_name;
+        TextView item_short_text;
+        TextView item_full_text;
+        TextView item_create_data;
+        TextView item_change_data;
+        CheckBox is_done;
 
-        public MyViewHolder(CoordinatorLayout v) {
+        MyViewHolder(CoordinatorLayout v) {
             super(v);
-            coordinatorLayout = v;
+            item_task_name = v.findViewById(R.id.item_task_name);
+            item_short_text = v.findViewById(R.id.item_short_text);
+            item_full_text = v.findViewById(R.id.item_full_text);
+            item_create_data = v.findViewById(R.id.item_create_data);
+            item_change_data = v.findViewById(R.id.item_change_data);
+            is_done = v.findViewById(R.id.is_done);
+        }
+
+        void update(Task task) {
+            item_task_name.setText(task.getName());
+            item_short_text.setText(task.getShortText());
+            item_full_text.setText(task.getFullText());
+            item_create_data.setText(task.getDateCreate());
+            item_change_data.setText(task.getDateChange());
+             if (task.getIsDone()) {
+                 is_done.setChecked(true);
+             } else {
+                 is_done.setChecked(false);
+             }
         }
     }
 }
--- app/src/main/java/nsu/android/todolist/Presenter/PresenterList.java
+++ app/src/main/java/nsu/android/todolist/Presenter/PresenterList.java
@@ -2,8 +2,6 @@ package nsu.android.todolist.Presenter;
 
 import android.content.Intent;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.CheckBox;
@@ -24,7 +22,7 @@ public class PresenterList {
     private NotesList notesList;
     private SQLiteStorage sqliteStorage;
 
-    private RecyclerView taskList;
+    private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
@@ -43,42 +41,13 @@ public class PresenterList {
         notesList.startActivity(intent);
     }
 
-
     public void onCreateEvent() {
-        taskList = notesList.findViewById(R.id.notes_recyc);
-
+        recyclerView = notesList.findViewById(R.id.notes_recyc);
         layoutManager = new LinearLayoutManager(notesList);
-        taskList.setLayoutManager(layoutManager);
-
+        recyclerView.setLayoutManager(layoutManager);
         List<Task> tasks = sqliteStorage.getAllTasks();
 
-        CoordinatorLayout c = notesList.findViewById(R.id.coordinator);
-        List<CoordinatorLayout> myDataset = new ArrayList<>();
-
-        for (Task t : tasks) {
-            TextView item_task_name =  notesList.findViewById(R.id.item_task_name);
-            TextView item_short_text = notesList.findViewById(R.id.item_short_text);
-            TextView item_full_text = notesList.findViewById(R.id.item_full_text);
-            TextView item_create_data = notesList.findViewById(R.id.item_create_data);
-            TextView item_change_data = notesList.findViewById(R.id.item_change_data);
-            CheckBox is_done = notesList.findViewById(R.id.is_done);
-
-            item_task_name.setText(t.getName());
-            item_short_text.setText(t.getShortText());
-            item_full_text.setText(t.getFullText());
-            item_create_data.setText(t.getDateCreate());
-            item_change_data.setText(t.getDateChange());
-
-            if (t.getIsDone()) {
-                is_done.setChecked(true);
-            } else {
-                is_done.setChecked(false);
-            }
-
-            myDataset.add(c);
-        }
-
-        mAdapter = new MyAdapter(myDataset);
-        taskList.setAdapter(mAdapter);
+        mAdapter = new MyAdapter(tasks);
+        recyclerView.setAdapter(mAdapter);
     }
 }
